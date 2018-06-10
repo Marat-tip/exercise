070e2252fe04281877337ac9369dc122a6db6e81
Marat Kazgozhin <ad.gaming.proger@gmail.com>
2018-06-10 19:18:02 +0600

    added configuration and influx integration

--- src/main/java/com/kcell/exercises/Document.java
+++ src/main/java/com/kcell/exercises/Document.java
@@ -1,7 +1,24 @@
 package com.kcell.exercises;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.influxdb.annotation.Column;
+import org.influxdb.annotation.Measurement;
+
+import java.time.Instant;
+
+@Measurement(name = "docs")
 public class Document {
+    public static Document DEFAULT = new Document(0);
+
+    @Column(name = "value")
     private Integer value;
+
+    @Column(name = "time")
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
+    private Instant time;
+
+    public Document() {
+    }
 
     public Document(Integer value) {
         this.value = value;
@@ -11,7 +28,7 @@ public class Document {
         return value;
     }
 
-    public void setValue(Integer value) {
-        this.value = value;
+    public Instant getTime() {
+        return time;
     }
 }
--- src/main/java/com/kcell/exercises/DocumentController.java
+++ src/main/java/com/kcell/exercises/DocumentController.java
@@ -1,15 +1,44 @@
 package com.kcell.exercises;
 
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.influxdb.impl.InfluxDBResultMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.influxdb.InfluxDBTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/documents/")
 public class DocumentController {
 
+    private static final String SELECT = "Select LAST(value),\"value\" from docs";
+
+    @Value("${spring.influxdb.database}")
+    private String database;
+
+    @Autowired
+    private InfluxDBTemplate<Point> influxDBTemplate;
+    private InfluxDBResultMapper mapper = new InfluxDBResultMapper();
+
     @RequestMapping(method = RequestMethod.GET, value = "/last")
-    public Document getLastDocument(){
-        return new Document(100);
+    public Document getLastDocument() {
+        List<Document> documents = mapper.toPOJO(influxDBTemplate.query(new Query(SELECT, database)), Document.class);
+        return documents.stream()
+                .findFirst()
+                .orElse(Document.DEFAULT);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/last")
+    public Document getLastDocument(@RequestBody Document document) {
+        influxDBTemplate.write(Point.measurement("docs")
+                .addField("value", document.getValue())
+                .build());
+        return getLastDocument();
     }
 }
